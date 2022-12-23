7f42abcb2b0531a1e5c7c7197a556c9ea6d3e71b
msamuel52581 <msamuel52581@gmail.com>
2022-12-22 20:26:49 -0500

    added options container to js removed questions in html

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,43 @@
-window.onload = () => {
-let timer = 100;
-let startButton = docuemnt.querySelector('#startButton');
-startButton.addEventListener ('click', () => { 
-    startQuiz(timer);
-})
-let timerInterval = startTimer(timer);
+const questions = [
+    {
+    question: 'What is HTML?',
+    option1: 'Programing language',
+    option2: 'Scripting language',
+    option3: 'Style sheet language',
+    option4: 'Markup language',
+    answer: 'Markup language',
+    },
+    {
+    question: 'What is CSS?',
+    option1: 'Programing language',
+    option2: 'Scripting language',
+    option3: 'Markup language',
+    option4: 'Style sheet language',
+    answer: 'Style sheet language', 
+    },
+];
 
-const end= document.querySelector("#end")
+window.onload = () => {
+    let timer = 100;
+    let startButton = docuemnt.querySelector('#startButton');
+    let timerInterval;
+    startButton.addEventListener ('click', () => { 
+        timerInterval - startTimer(timer)
+        startQuiz(timer);
+});
+
+const end = document.querySelector("#end")
 end.addEventListener('click', () => {
     stopTimer(timerInterval);
 })
 };
 
-const startQuiz = (timer) => {
+const startTimer = (timer) => {
     const timerDisplay = document.querySelector('#timer')
     const timerInterval = setInterval(() => { 
         timer --;
         timerDisplay.textContent = 'Timer: ' + timer;
-    } 1000);
+    }, 1000);
     return timerInterval;
 }
     
@@ -25,8 +45,18 @@ const startQuiz = () => {
 const mainContent = document.querySelector('#mainContent');
 mainContent.classList.add('hide');
 
+let i= 0;
 const questionContainer = document.querySelector('#question-container');
 questionContainer.classList.remove('hide');
+const question= document.querySelector("#question")
+question.textContent = questions[i].question;
+const optionsContainer = document.querySelector('#options-container');
+optionsContainer.innerHTML = '';
+for(let j=0; j<4; j++ ){
+    const option= document.createElement("input");
+    option.textContent(question [i]["option"+(j+1)]);
+    questionContainer.appendChild(option);
+}
 };
 
 const stopTimer = (timerInterval) => {
